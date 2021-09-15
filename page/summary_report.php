15065e31f1438748445c2f992a4d79dcae0e6dd8
prince-i <prince_arce01@outlook.com>
2021-09-15 17:01:37 +0800

    HRARIS

--- page/summary_report.php
+++ page/summary_report.php
@@ -294,7 +294,16 @@
                 },success:function(data){
                     // console.log(data);
                     $('#absence_per_section').html(data);
+                    var section_label = [];
+                    var section_data = [];
+                    $('.section_label').each(function(){
+                        section_label.push($(this).html());
+                    });
+                    $('.section_data').each(function(){
+                        section_data.push($(this).html());
+                    });
 
+                    generate_chart_per_section(section_label,section_data);
                 }
             });
         }
@@ -455,6 +464,58 @@
             });
         }
 
+        const generate_chart_per_section =(x,y)=>{
+            var ctx = document.getElementById('per_section_chart');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: x,
+                    datasets: [{
+                        label: '# of Absences',
+                        data: y,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    },
+                    indexAxis: 'y'
+                   
+                }
+                                 
+            });
+        }
     </script>
 </body>
 
