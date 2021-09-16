d5885f22784a85b4f0e5aa2795c96bece090d15b
prince-i <prince_arce01@outlook.com>
2021-09-16 16:37:48 +0800

    HR ARIS

--- test.php
+++ test.php
@@ -8,14 +8,15 @@
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     foreach($stmt->fetchALL() as $section){
+        $section_name = $section['section'];
         echo '<tbody>';
         echo '<tr>';
         echo '<td>'.$section['section'].'</td>';
-        $get_reason = "SELECT DISTINCT reason FROM aris_absent_filing ORDER BY reason ASC";
+        $get_reason = "SELECT DISTINCT reason,count(id) as countid FROM aris_absent_filing WHERE section = '$section_name' GROUP BY reason ORDER BY reason ASC";
         $stmt2 = $conn->prepare($get_reason);
         $stmt2->execute();
         foreach($stmt2->fetchALL() as $reason){
-            echo '<td>'.$reason['reason'].'</td>';
+            echo '<td>'.$reason['reason'].'=>'.$reason['countid'].'</td>';
         }
         echo '</tr>';
         echo '</tbody>';
